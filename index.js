59b7c850a5be13be7cb66addf81ed214d39bdc65
r3ot <129449887+r3ot@users.noreply.github.com>
2025-07-21 14:52:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+        let menuList = document.getElementById("menuList")
+        menuList.style.maxHeight = "0px";
+
+        function toggleMenu(){
+            if(menuList.style.maxHeight == "0px")
+            {
+                menuList.style.maxHeight = "300px";
+            }
+            else{
+                menuList.style.maxHeight = "0px";
+            }
+        }
+        
+    <script src="https://kit.fontawesome.com/f8e1a90484.js" crossorigin="anonymous"></script>
